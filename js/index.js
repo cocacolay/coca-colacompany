ad06db2ab1e26b29de97b01068f8f88c7c5a334a
lisahuele <lisahuele@gmail.com>
2021-03-20 12:18:29 -0400

    added "ourcompany" page

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 $(function(){
     $(document).scroll(function(){
-         if($(this).scrollTop() >= $('#ourCompany').offset().top) {
+         if($(this).scrollTop() >= $('.sticky').offset().top) {
               $(".navbar").addClass("navbar-stuck");
               $(".logo").attr("src", "images/Logo.png");
               $(".search").attr("src", "images/search.png");
               $(".globe").attr("src", "images/world-icon.png");
               $(".menu__btn").addClass("menu__btn-black");
-
-
-
-              
+       
           } else {
               $(".navbar").removeClass("navbar-stuck");
               $(".logo").attr("src", "images/logo-white.png");
