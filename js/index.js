82f9d4001c5034fb1bf93e90245e2bbc944976d5
lisahuele <lisahuele@gmail.com>
2021-03-24 12:20:57 -0400

    added scroll-top button

--- js/index.js
+++ js/index.js
@@ -17,3 +17,25 @@ $(function(){
           }
       });
     });
+
+    
+    //back to top button
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
